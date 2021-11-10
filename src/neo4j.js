f527b48a90ccb4b3090b1bad7259ec4129fe04af
adam-cowley <adam@adamcowley.co.uk>
2021-11-10 20:58:22 +0000

    Completed Adding the Driver

--- src/neo4j.js
+++ src/neo4j.js
@@ -1,4 +1,5 @@
-// TODO: Import the neo4j-driver dependency
+// Import the neo4j dependency from neo4j-driver
+import neo4j from 'neo4j-driver'
 
 /**
  * A singleton instance of the Neo4j Driver to be used across the app
@@ -20,7 +21,12 @@ let driver
  */
 // tag::initDriver[]
 export function initDriver(uri, username, password) {
-  // TODO: Create an instance of the driver here
+  driver = neo4j.driver(uri, neo4j.auth.basic(username, password))
+
+  // Verify connectivity
+  return driver.verifyConnectivity()
+    // Resolve with an instance of the driver
+    .then(() => driver)
 }
 // end::initDriver[]
 
