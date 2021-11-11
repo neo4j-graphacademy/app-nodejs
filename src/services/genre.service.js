5354c6db2f37da043adfa74e87ce0696f181fbe3
adam-cowley <adam@adamcowley.co.uk>
2021-11-11 21:56:47 +0000

    Completed lesson: Browsing Genres

--- src/services/genre.service.js
+++ src/services/genre.service.js
@@ -37,11 +37,32 @@ export default class GenreService {
    */
   // tag::all[]
   async all() {
-    // TODO: Open a new session
-    // TODO: Get a list of Genres from the database
-    // TODO: Close the session
+    // Open a new Session
+    const session = this.driver.session()
 
-    return genres
+    // Get a list of Genres from the database
+    const res = await session.readTransaction(tx => tx.run(`
+      MATCH (g:Genre)<-[:IN_GENRE]-(m:Movie)
+      WHERE exists(m.imdbRating) AND exists(m.poster) AND g.name <> '(no genres listed)'
+      WITH g, m
+      ORDER BY m.imdbRating DESC
+
+      WITH g, head(collect(m)) AS movie
+
+      RETURN g {
+        link: '/genres/'+ g.name,
+        .name,
+        movies: size((g)<-[:IN_GENRE]-()),
+        poster: movie.poster
+      } AS genre
+      ORDER BY g.name ASC
+    `))
+
+    // Close the session
+    await session.close()
+
+    // Return results
+    return res.records.map(row => toNativeTypes(row.get('genre')))
   }
   // end::all[]
 
