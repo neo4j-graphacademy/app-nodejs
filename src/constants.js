dad9c9a29e15cd6e236a4ebcb94306b80e5fdcb5
yuchwang <37077496+yuchwang@users.noreply.github.com>
2022-03-20 13:30:00 -0400

    Update constants.js
    Seems like the constrants.js file does not run config() so that the APP_PORT variable will not be modified by the .env file.

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,6 @@
+import { config } from 'dotenv'
+config();
+
 export const APP_PORT = process.env.APP_PORT || 3000
 export const JWT_SECRET = process.env.JWT_SECRET || 'a secret key'
 export const SALT_ROUNDS = process.env.SALT_ROUNDS || 10
