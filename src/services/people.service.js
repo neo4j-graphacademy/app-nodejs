fa74bfcbe5c254219d9e49f5fc7cdae8dd6d00f5
Adam Cowley <adam@adamcowley.co.uk>
2023-04-24 09:17:14 +0100

    Update queries to use count subquery

--- src/services/people.service.js
+++ src/services/people.service.js
@@ -81,8 +81,8 @@ export default class PeopleService {
         MATCH (p:Person {tmdbId: $id})
         RETURN p {
           .*,
-          actedCount: size((p)-[:ACTED_IN]->()),
-          directedCount: size((p)-[:DIRECTED]->())
+          actedCount: count { (p)-[:ACTED_IN]->() },
+          directedCount: count { (p)-[:DIRECTED]->() }
         } AS person
       `, { id })
     )
