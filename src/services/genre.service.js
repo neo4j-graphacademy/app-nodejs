a199decc23dc79a491742dcab268ae1de71178ce
Martin O'Hanlon <martin.ohanlon@neo4j.com>
2025-03-21 15:14:21 +0000

    add all function

--- src/services/genre.service.js
+++ src/services/genre.service.js
@@ -37,11 +37,33 @@ export default class GenreService {
    */
   // tag::all[]
   async all() {
-    // TODO: Open a new session
-    // TODO: Get a list of Genres from the database
-    // TODO: Close the session
+    // Open a new Session
+    const session = this.driver.session()
 
-    return genres
+    // Get a list of Genres from the database
+    const res = await session.executeRead(tx => tx.run(`
+      MATCH (g:Genre)
+      WHERE g.name <> '(no genres listed)'
+      CALL {
+        WITH g
+        MATCH (g)<-[:IN_GENRE]-(m:Movie)
+        WHERE m.imdbRating IS NOT NULL
+        AND m.poster IS NOT NULL
+        RETURN m.poster AS poster
+        ORDER BY m.imdbRating DESC LIMIT 1
+      }
+      RETURN g {
+        .*,
+        poster: poster
+      } as genre
+      ORDER BY g.name ASC
+    `))
+
+    // Close the session
+    await session.close()
+
+    // Return results
+    return res.records.map(row => toNativeTypes(row.get('genre')))
   }
   // end::all[]
 
